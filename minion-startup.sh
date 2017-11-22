b5e67643e2a57b972761d427c3a89cc36f6a1724
unknown <angelanieboer@gmail.com>
2017-11-22 11:00:23 +0100

    Fixed typo

--- minion-startup.sh
+++ minion-startup.sh
@@ -11,7 +11,7 @@ echo "127.0.1.1 $(hostname)" >> /etc/hosts
 #Getting the saltstack public key to add to apt
 wget -O - https://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
 #Copying the repo to a source list
-sh -c 'echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" >> /etc/apt/sources.list.d/saltstack.list'
+sh -c 'echo "deb http://repo.saltstack.com/apt/ubuntu/16.04/amd64/latest xenial main" > /etc/apt/sources.list.d/saltstack.list'
 #updating apt so it includes the new source
 apt-get update -y
 
